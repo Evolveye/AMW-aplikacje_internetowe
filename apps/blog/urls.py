563fe3f41276070b28f738520091d467892a5990
Evolveye <stolarski.paw@gmail.com>
2020-11-13 00:45:27 +0100

    Add post form

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path( "post/<slug:slug>/", views.PostDetail.as_view(), name="post_detail" ),
+    path( "posts/new/", views.post_new, name="post_new" ),
     path( "", views.PostList.as_view(), name="home" ),
-    path( "<slug:slug>/", views.PostDetail.as_view(), name="post_detail" ),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.template.defaultfilters import slugify
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.views import generic
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +14,26 @@ class PostList( generic.ListView ):
 class PostDetail( generic.DetailView ):
     model = Post
     template_name = "post_detail.html"
+
+def post_new( request ):
+    if request.method == "POST":
+        form = PostForm( request.POST )
+        slug = slugify( form.data[ "title" ] )
+
+        if Post.objects.filter( slug=slug ).first() != None:
+            form.add_error( "title", "Slug made from passed title is not unique!" )
+
+        elif form.is_valid():
+            post = form.save( commit=False )
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.status = 1
+            post.slug = slug
+            post.save()
+
+            return redirect( "post_detail", slug=post.slug )
+
+    else:
+        form = PostForm()
+
+    return render( request, "post_edit.html", { "form":form } )
