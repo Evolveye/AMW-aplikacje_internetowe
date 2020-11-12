1957cace372d6554a9fefded0b76809a2e4a067f
Evolveye <stolarski.paw@gmail.com>
2020-11-12 16:36:17 +0100

    Posts rendering

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -22,3 +22,12 @@ class Post( models.Model ):
 
     def __str__( self ):
         return self.title
+
+    def get_absolute_url( self ):
+        return reverse(
+            "questions:detail",
+            kwargs={
+                "slug": self.slug,
+                "pk": self.pk
+            }
+        )
