4483ac8b1a2539248d17663c7195f6807c524555
Evolveye <stolarski.paw@gmail.com>
2020-11-12 17:08:03 +0100

    os.path.join for STATICFILES_DIRS

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join( BASE_DIR, 'staticfiles' )
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
