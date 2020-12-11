aac99ef6c1fd6f26e33729305dbd8ffed66067c7
Evolveye <stolarski.paw@gmail.com>
2020-12-11 20:43:50 +0100

    Static files temp fix

--- apps/apps/views.py
+++ apps/apps/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from django.urls import get_resolver
 
+from datetime import datetime
+
 def root( request ):
-    return render( request, "index.html" )
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, we default to zero and cast that.
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    reset_last_visit_time = False
+    response = render(request, 'index.html', { "visits":visits })
+    # Does the cookie last_visit exist?
+    if 'last_visit' in request.COOKIES:
+        # Yes it does! Get the cookie's value.
+        last_visit = request.COOKIES['last_visit']
+        # Cast the value to a Python date/time object.
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            # ...and flag that the cookie last visit needs to be updated
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so flag that it should be set.
+        reset_last_visit_time = True
+
+        #Obtain our Response object early so we can add cookie information.
+        response = render(request, 'index.html', { "visits":visits } )
+
+    if reset_last_visit_time:
+        response.set_cookie('last_visit', datetime.now())
+        response.set_cookie('visits', visits)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
