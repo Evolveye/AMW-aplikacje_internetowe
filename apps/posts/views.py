aca2fba4ee78d53fe2ab30cb13bca3d42c34dc50
Evolveye <stolarski.paw@gmail.com>
2020-12-11 19:04:58 +0100

    Authorized API views

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -17,20 +17,24 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
-"""
 class PostList(generics.ListCreateAPIView):
     # permission_classes = (permissions.IsAuthenticated,) # View-Level Permissions
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = (permissions.IsAuthenticated,)
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
 class UserList(generics.ListCreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-"""
